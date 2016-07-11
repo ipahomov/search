18659f0635cc4c5e3dfe82788a27b34cc8dd0746
Igor <p@m.com>
2016-07-11 21:38:25 +0300

    linkComp

---
+++ src/main/java/com/search/task/services/LinkComp.java
@@ -0,0 +1,19 @@
+package com.search.task.services;
+
+import java.util.Comparator;
+
+/**
+ * Comparator for links
+ * Created by IPahomov on 11.07.2016.
+ */
+public class LinkComp implements Comparator<Link> {
+    public int compare(Link o1, Link o2) {
+        int result = o1.getUrl().compareTo(o2.getUrl());
+        if(result==0){
+            return result;
+        } else {
+            return Double.compare(o1.getAverage(), o2.getAverage());
+        }
+    }
+
+}
